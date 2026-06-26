517e5ff631d99d1d7598323996e3147fc8d7e437
Acia Alam <alamacia1@gmail.com>
2026-06-26 14:04:12 +0600

    Update UHI_LST_Analysis.js

--- scripts/UHI_LST_Analysis.js
+++ scripts/UHI_LST_Analysis.js
@@ -11,7 +11,7 @@ return image.addBands(opticalBands, null, true)
 
 // Filter the collection, first by the aoi, and then by date.
 var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2') // dataset
-.filterDate('2022-03-01','2022-05-01') // change this time period by your own
+.filterDate('2026-03-01','2026-05-01') // change this time period by your own
 .filterBounds(aoi)
 .map(maskL8sr)
 .map(applyScaleFactors)
@@ -65,7 +65,7 @@ Map.addLayer(lst_celsius, lst_vis, 'LST')
 
 // Export.image.toDrive({
 //   image: lst_land,
-//   description: 'Dhaka_2022_lst',
+//   description: 'Dhaka_2026_lst',
 //   scale: 30,
 //   maxPixels: 1e13,
 //   folder: 'GEE',
@@ -128,7 +128,7 @@ Map.addLayer(uhi.clip(aoi), uhi_vis, 'UHI')
 
 // Export.image.toDrive({
 //   image: uhi,
-//   description: 'Dhaka_2022_uhi',
+//   description: 'Dhaka_2026_uhi',
 //   scale: 30,
 //   maxPixels: 1e13,
 //   folder: 'GEE',
